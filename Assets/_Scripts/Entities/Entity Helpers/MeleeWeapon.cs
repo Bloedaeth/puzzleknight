19e5764df1ef146329bc08128a56167129236777
Sean Harrison <seanharrs97@hotmail.com>
2017-07-26 11:57:31 +1000

    Enemies can now be attacked again!!!

--- Assets/_Scripts/Entities/Entity Helpers/MeleeWeapon.cs
+++ Assets/_Scripts/Entities/Entity Helpers/MeleeWeapon.cs
@@ -10,12 +10,16 @@ public class MeleeWeapon : MonoBehaviour
     private new AudioSource audio;
     private AudioClip[] attackSounds;
 
-    private int attackStateHash;
+    private int attackStateOneHash;
+    private int attackStateTwoHash;
+    private int attackStateThreeHash;
 
     private void Awake()
     {
         anim = Self.GetComponent<Animator>();
-        attackStateHash = Animator.StringToHash("Base Layer.Attack");
+        attackStateOneHash = Animator.StringToHash("Base Layer.Attack.Attack Combo 1");
+        attackStateTwoHash = Animator.StringToHash("Base Layer.Attack.Attack Combo 2");
+        attackStateThreeHash = Animator.StringToHash("Base Layer.Attack.Attack Combo 3");
 
         audio = GetComponent<AudioSource>();
         attackSounds = Self.GetComponent<EntitySoundsCommon>().attackSounds;
@@ -31,7 +35,9 @@ public class MeleeWeapon : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
-        if(state.fullPathHash != attackStateHash)
+        if(state.fullPathHash != attackStateOneHash &&
+           state.fullPathHash != attackStateTwoHash &&
+           state.fullPathHash != attackStateThreeHash)
             return;
 
         Entity target = collision.gameObject.GetComponent<Entity>();
