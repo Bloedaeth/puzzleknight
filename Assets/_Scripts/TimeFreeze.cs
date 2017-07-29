23de8879948a9eb8d7dd427a6c199104b174d40e
Sean Harrison <seanharrs97@hotmail.com>
2017-07-29 12:28:57 +1000

    collider and script now on correct objects, time slow working

--- Assets/_Scripts/TimeFreeze.cs
+++ Assets/_Scripts/TimeFreeze.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeFreeze : MonoBehaviour
@@ -8,9 +7,7 @@ public class TimeFreeze : MonoBehaviour
     public static float FROZEN_TIME_SCALE = 0.1f;
 
     /// <summary>The game object that determines where time should freeze.</summary>
-    public SphereCollider TimeFreezeCollider;
-
-    private List<GameObject> frozenObjects = new List<GameObject>();
+    public SphereCollider FreezeRadiusCollider;
 
     /// <summary>Freezes/Slows time in a set radius around the casting point for a set time.</summary>
     /// <param name="time">How long the effect will last.</param>
@@ -22,41 +19,12 @@ public class TimeFreeze : MonoBehaviour
 
     private IEnumerator ActivateFreeze(float time, float radius)
     {
-        TimeFreezeCollider.transform.position = transform.position + transform.forward + new Vector3(0, 1, 0);
-        TimeFreezeCollider.radius = radius;
-        TimeFreezeCollider.gameObject.SetActive(true);
+        FreezeRadiusCollider.transform.position = transform.position + transform.forward + new Vector3(0, 1, 0);
+        FreezeRadiusCollider.radius = radius;
+        FreezeRadiusCollider.gameObject.SetActive(true);
 
         yield return new WaitForSeconds(time);
-        
-        for(int i = 0; i < frozenObjects.Count; ++i)
-        {
-            frozenObjects[i].GetComponent<Enemy>().SlowedTime = false;
-            frozenObjects.Remove(frozenObjects[i]);
-        }
-        TimeFreezeCollider.gameObject.SetActive(false);
-    }
 
-    private void OnTriggerStay(Collider other)
-    {
-        Debug.Log(other.name);
-        Enemy enemy = other.GetComponent<Enemy>();
-        if(enemy == null)
-            return;
-
-        if(enemy.SlowedTime)
-            return;
-
-        enemy.SlowedTime = true;
-        frozenObjects.Add(other.gameObject);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        Enemy enemy = other.GetComponent<Enemy>();
-        if(enemy == null)
-            return;
-
-        enemy.SlowedTime = false;
-        frozenObjects.Remove(other.gameObject);
+        FreezeRadiusCollider.GetComponent<TimeFreezeCollider>().EndFreeze();
     }
 }
