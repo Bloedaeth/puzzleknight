dbadd151080d7fc6b963f2278b7fd9192836c992
Sol <soldasilva_21@hotmail.com>
2017-07-31 03:35:38 +1000

    Turrets, Moving Platforms and Force Field Added
    Turrets have been added into the scene for extra challenge and deplete the player's health if hit.  Animations added to playforms for movement.  Player must hit a button to disable the Force Field

---
+++ Assets/_Scripts/Destroy_Forcefield.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Forcefield : MonoBehaviour {
+    public GameObject other;
+    void OnCollisionEnter(Collision collidingObject)
+    {
+        
+        //If collidingObject is an Enemy
+        if (collidingObject.gameObject.tag == "Player")
+        {
+            Destroy(other);
+        }
+
+        ;
+    }
+}
+
+
