c804e18394662428d002e211ceb973e42a0836be
Sean Harrison <seanharrs97@hotmail.com>
2017-07-30 19:58:46 +1000

    add item to platform that is reset if morpheus dies, added lava pit

--- Assets/_Scripts/RespawnColliderScript.cs
+++ Assets/_Scripts/RespawnColliderScript.cs
@@ -5,8 +5,23 @@ public class RespawnColliderScript : MonoBehaviour
 {
     public Transform SpawnPoint;
 
+    public bool IsPressurePlateRespawn;
+
+    private GameObject crystal;
+
+    private void Awake()
+    {
+        if(IsPressurePlateRespawn)
+            crystal = FindObjectOfType<Crystal>().gameObject;
+    }
     private void OnTriggerEnter(Collider other)
     {
-		if (other.tag == "Player") other.transform.position = SpawnPoint.position;
+        if(other.CompareTag("Player"))
+        {
+            other.transform.position = SpawnPoint.position;
+
+            if(IsPressurePlateRespawn)
+                crystal.SetActive(true);
+        }
     }
 }
