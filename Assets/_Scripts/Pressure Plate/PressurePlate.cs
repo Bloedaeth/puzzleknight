7a3c634ac9e542fdfe2e266eb7adfb01fb39309c
Sean Harrison <seanharrs97@hotmail.com>
2017-08-10 13:14:10 +1000

    disabled time freeze (and dead enemies) no longer activate pressure plate

--- Assets/_Scripts/Pressure Plate/PressurePlate.cs
+++ Assets/_Scripts/Pressure Plate/PressurePlate.cs
@@ -5,6 +5,7 @@ public class PressurePlate : MonoBehaviour
     private int numEntitiesOnPlate;
 
     private RaisePlatform platform;
+    private bool triggered = false;
 
     private void Awake()
     {
@@ -13,22 +14,32 @@ public class PressurePlate : MonoBehaviour
 
     private void Update()
     {
-        if(numEntitiesOnPlate > 0)
-        {
-            if(!platform.PressurePlateActive)
-                platform.PressurePlateActive = true;
-        }
-        else if(platform.PressurePlateActive)
+        if(!triggered)
             platform.PressurePlateActive = false;
+        triggered = false;
+        //if(numEntitiesOnPlate > 0)
+        //{
+        //    if(!platform.PressurePlateActive)
+        //        platform.PressurePlateActive = true;
+        //}
+        //else if(platform.PressurePlateActive)
+        //    platform.PressurePlateActive = false;
     }
 
-    private void OnTriggerEnter(Collider o)
+    private void OnTriggerStay(Collider other)
     {
-        ++numEntitiesOnPlate;
+        triggered = true;
+        if(!platform.PressurePlateActive)
+            platform.PressurePlateActive = true;
     }
 
-    private void OnTriggerExit(Collider o)
-    {
-        --numEntitiesOnPlate;
-    }
+    //private void OnTriggerEnter(Collider o)
+    //{
+    //    ++numEntitiesOnPlate;
+    //}
+
+    //private void OnTriggerExit(Collider o)
+    //{
+    //    --numEntitiesOnPlate;
+    //}
 }
