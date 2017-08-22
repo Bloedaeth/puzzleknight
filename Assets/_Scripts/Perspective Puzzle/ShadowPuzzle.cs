434560ff6195f19eea6927a876817852f9556ce0
StevenMacHunter <anim8sme@gmail.com>
2017-08-23 06:13:19 +1000

    Small changes, ready to merge for now

--- Assets/_Scripts/Perspective Puzzle/ShadowPuzzle.cs
+++ Assets/_Scripts/Perspective Puzzle/ShadowPuzzle.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class ShadowPuzzle : Puzzle {
 
-	/// <summary>
-	/// The trigger zone hit variable is used to determine if the trigger zone at the end of the puzzle has been hit, to open the door.
-	/// </summary>
-	private bool triggerZoneHit;
-
-	// Use this for initialization
-	void Start () {
-		triggerZoneHit = false;
-	}
 
 	void OnTriggerEnter(Collider o) {
 		if (o.transform.tag == "Player") {
