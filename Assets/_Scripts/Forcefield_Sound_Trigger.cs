f1bdf9b4a9c44e3099a3dcbffc16885c6533c8d4
Sol <soldasilva_21@hotmail.com>
2017-08-27 02:33:20 +1000

    Lights + Platforms Resized

---
+++ Assets/_Scripts/Forcefield_Sound_Trigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcefield_Sound_Trigger : MonoBehaviour {
+    public AudioClip otherClip;
+    void OnTriggerEnter(Collider other)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        audio.Play(44100);
+    }
+}
