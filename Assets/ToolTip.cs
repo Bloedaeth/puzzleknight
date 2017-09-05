337e1cb8d85a8d6b3707c1611ab656e1152e2fd8
Sean Harrison <seanharrs97@hotmail.com>
2017-09-05 17:16:51 +1000

    start work making image follow mouse cursor

--- Assets/ToolTip.cs
+++ Assets/ToolTip.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class ToolTip : MonoBehaviour {
+public class ToolTip : MonoBehaviour
+{
+    public Sprite[] descriptions;
 
-	public Sprite[] descriptions;
+    public Image img;
 
-	public Image img;
+    public void Display(Sprite sprite)
+    {
+        img.sprite = sprite;
+        img.color = new Color(img.color.r, img.color.g, img.color.b, sprite == null ? 0 : 255);
+    }
 
-	public void Display(Sprite sprite)
-	{
-		Debug.Log(sprite);
-		img.sprite = sprite;
-		img.color = new Color(img.color.r, img.color.g, img.color.b, sprite == null ? 0 : 255);
-	}
+    public void DisplayNull()
+    {
+        img.sprite = null;
+        img.color = new Color(img.color.r, img.color.g, img.color.b, 0);
+    }
+
+    private void OnGUI()
+    {
+        if(img.sprite != null)
+        {
+            transform.localPosition = Event.current.mousePosition;
+        }
+    }
 }
