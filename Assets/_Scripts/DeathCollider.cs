c6fa3cbdf1c24cb0fd293bc04a5b34e0767e73bd
Sean Harrison <seanharrs97@hotmail.com>
2017-09-11 19:31:58 +1000

    add old unaltered camera clip script, make death collider ignore entities with the DeathAnimation component. Fixes #86.

--- Assets/_Scripts/DeathCollider.cs
+++ Assets/_Scripts/DeathCollider.cs
@@ -5,6 +5,9 @@ public class DeathCollider : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        if(other.GetComponent<DeathAnimation>())
+            return;
+
         Health hp = other.GetComponent<Health>();
         if(hp)
             hp.TakeDamage(hp.InitialAndMaxHealth);
