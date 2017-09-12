e0a143a9c84227795d5a93209fc8d3cd0dcd30f9
Sean Harrison <seanharrs97@hotmail.com>
2017-09-12 23:33:56 +1000

    lever animation plays, but seems to "teleport" when flipping back

--- Assets/_Scripts/Boss Fight/Lever.cs
+++ Assets/_Scripts/Boss Fight/Lever.cs
@@ -1,20 +1,16 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animation))]
 public class Lever : MonoBehaviour
 {
-    //public Material red;
-    //public Material green;
     public Pylon pylon;
-
-    //private new Renderer renderer;
-    private Animator animator;
+    
+    private Animation anim;
     private bool active;
 
     private void Start()
     {
-        //renderer = GetComponent<Renderer>();
-        animator = GetComponent<Animator>();
+        anim = GetComponent<Animation>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,7 +24,6 @@ public class Lever : MonoBehaviour
     {
         if(active && Input.GetKeyDown(KeyCode.E))
         {
-            //renderer.material = red;
             pylon.SetPylonActive(false);
             SetLeverActive(false);
         }
@@ -44,9 +39,7 @@ public class Lever : MonoBehaviour
     public void SetLeverActive(bool val)
     {
         active = val;
-        animator.Play("Flip Lever");
-        animator.speed *= -1;
-        //animation["Flip Lever"].speed *= -1;
-        //renderer.material = active ? green : red;
+        anim.Play("Flip Lever");
+        anim["Flip Lever"].speed *= -1;
     }
 }
