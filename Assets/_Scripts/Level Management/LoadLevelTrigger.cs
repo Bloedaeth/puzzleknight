491427e18cfe848682ed2deabc8c3c2b1a88c8de
Sean Harrison <seanharrs97@hotmail.com>
2017-09-13 10:36:30 +1000

    Add trigger to load Deliverable from Tutorial, game runs from start menu to tutorial to deliverable

---
+++ Assets/_Scripts/Level Management/LoadLevelTrigger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class LoadLevelTrigger : MonoBehaviour
+{
+    public LevelManager lm;
+
+    private void OnTriggerEnter(Collider other) { lm.LoadNextLevel(); }
+}
