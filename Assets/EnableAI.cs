712d97fff02d5ac798d4470e19d149c69cbb0b80
Sean Harrison <seanharrs97@hotmail.com>
2017-09-16 21:08:21 +1000

    disable grunt AI in tutorial until player reaches area, ensures the "swarm" effect actually happens

---
+++ Assets/EnableAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAI : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+
+        GruntEnemy[] enemies = FindObjectsOfType<GruntEnemy>();
+        foreach(GruntEnemy e in enemies)
+            e.enabled = true;
+
+        this.enabled = false;
+    }
+}
