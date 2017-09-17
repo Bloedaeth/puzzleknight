19ff70148b56e0d7c83187fcefbe3c265f224c59
Sean Harrison <seanharrs97@hotmail.com>
2017-09-18 01:35:14 +1000

    teleporter...actually...works when it should and not when it sohuldn't

--- Assets/_Scripts/Teleporter.cs
+++ Assets/_Scripts/Teleporter.cs
@@ -12,9 +12,14 @@ public class Teleporter : MonoBehaviour
     private Transform hub;
 	private Transform player;
 
+    private bool inEditor = false;
+
 	private void Awake()
 	{
-		if(instance == null)
+#if UNITY_EDITOR
+        inEditor = true;
+#endif
+        if(instance == null)
 		{
 			instance = this;
 			DontDestroyOnLoad(gameObject);
@@ -22,22 +27,13 @@ public class Teleporter : MonoBehaviour
 		else
 			Destroy(this);
 
-        if(!Debug.isDebugBuild)
-            FindTransforms();
-        else
-#if UNITY_EDITOR
-            FindTransforms();
-#endif
+        OnLevelWasLoaded();
     }
 
     private void OnLevelWasLoaded()
 	{
-        if(!Debug.isDebugBuild)
+        if(inEditor || Debug.isDebugBuild)
             FindTransforms();
-        else
-#if UNITY_EDITOR
-            FindTransforms();
-#endif
     }
 
     private void FindTransforms()
@@ -53,26 +49,9 @@ public class Teleporter : MonoBehaviour
 
     private void Update()
 	{
-        if(!Debug.isDebugBuild)
+        if(!(inEditor || Debug.isDebugBuild))
             return;
-        else
-#if UNITY_EDITOR
-        if(SceneManager.GetActiveScene().buildIndex == 3 && Input.GetKeyDown(KeyCode.Alpha0))
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        else
-        {
-            if(Input.GetKeyDown(KeyCode.Alpha6))
-                player.position = hub.position;
-            if(Input.GetKeyDown(KeyCode.Alpha7))
-                player.position = jumpPuzzle.position;
-            if(Input.GetKeyDown(KeyCode.Alpha8))
-                player.position = shadowPuzzle.position;
-            if(Input.GetKeyDown(KeyCode.Alpha9))
-                player.position = pressurePlatePuzzle.position;
-            if(Input.GetKeyDown(KeyCode.Alpha0))
-                player.position = bossFight.position;
-        }
-#endif
+
         if(SceneManager.GetActiveScene().buildIndex == 3 && Input.GetKeyDown(KeyCode.Alpha0))
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		else
