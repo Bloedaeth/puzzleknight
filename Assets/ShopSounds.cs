5c6293b36051b11d4d688f855766274df2e4bad6
Sean Harrison <seanharrs97@hotmail.com>
2017-09-18 19:58:58 +1000

    shopkeeper greeting/goodbye/purchase sounds placed in, added an extra hurt sound to goblin grunts

---
+++ Assets/ShopSounds.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ShopSounds : MonoBehaviour
+{
+    public AudioClip[] Greetings;
+    public AudioClip[] Goodbyes;
+    public AudioClip[] Purchases;
+
+    private new AudioSource audio;
+
+    private const float SOUND_COOLDOWN = 10f;
+
+    private float timeSinceGreet;
+    private float timeSinceGoodbye;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        timeSinceGreet += Time.deltaTime;
+        timeSinceGoodbye += Time.deltaTime;
+    }
+
+    public void PlayGreeting()
+    {
+        if(Greetings.Length == 0 || timeSinceGreet < SOUND_COOLDOWN)
+            return;
+
+        audio.clip = Greetings[Random.Range(0, Greetings.Length)];
+        audio.Play();
+
+        timeSinceGreet = 0f;
+    }
+
+    public void PlayGoodbye()
+    {
+        if(Goodbyes.Length == 0 || timeSinceGoodbye < SOUND_COOLDOWN)
+            return;
+
+        audio.clip = Goodbyes[Random.Range(0, Goodbyes.Length)];
+        audio.Play();
+
+        timeSinceGoodbye = 0f;
+    }
+
+    public void PlayPurchase()
+    {
+        if(Purchases.Length == 0)
+            return;
+
+        audio.clip = Purchases[Random.Range(0, Purchases.Length)];
+        audio.Play();
+    }
+}
