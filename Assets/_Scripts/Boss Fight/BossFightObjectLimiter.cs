4ce538497a9e4cba18c1aa7561e2322fa68e1c3d
StevenMacHunter <anim8sme@gmail.com>
2017-11-03 00:59:38 +1000

    Added object limiter to the boss fight, now the boss fight runs at 60FPS glory.

---
+++ Assets/_Scripts/Boss Fight/BossFightObjectLimiter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to disable active objects upon entry into the boss fight.
+/// </summary>
+
+public class BossFightObjectLimiter : MonoBehaviour {
+
+	Player p;
+	public GameObject[] sections;
+
+	bool limiting;
+
+	// Use this for initialization
+	void Start () {
+		if (sections.Length == 0) {
+			Debug.LogError ("Please set the Puzzle sections as the objects in this array;\n and any 'section' you want to disable when the boss fight starts.");
+		}
+
+		p = FindObjectOfType<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (p.InBossFight && !limiting) {
+
+			SetAllGameObjectsTo (limiting);
+
+			limiting = true;
+
+		} else if (!p.InBossFight && limiting) {
+			SetAllGameObjectsTo (limiting);
+
+			limiting = false;
+		}
+	}
+		
+
+	void SetAllGameObjectsTo(bool state) {
+		foreach (GameObject g in sections) {
+			g.SetActive (state);
+		}
+	}
+}
