85fdd10e8712cb4a9d38598dc84fa81c399506a2
StevenMacHunter <anim8sme@gmail.com>
2017-11-03 03:16:12 +1000

    Fully completely level optimiser.

--- Assets/_Scripts/Level Management/Optimisers/PuzzleSectionDisabler.cs
+++ Assets/_Scripts/Level Management/Optimisers/PuzzleSectionDisabler.cs
@@ -10,31 +10,69 @@ public class PuzzleSectionDisabler : MonoBehaviour {
 
 	bool[] currStates;
 
+	bool defaultsApplied = false;
+
+	void OnEnable() {
+		defaultsApplied = false;
+	}
+
 	// Use this for initialization
 	void Start () {
 		colliders = GetComponentsInChildren<DisablerID> ();
 		sections = GetComponentsInChildren<SectionID> ();
 		currStates = new bool[colliders.Length];
 
+
+
 		sectionObjects = new GameObject[sections.Length];
 
 		for (int i = 0; i < sections.Length; i++) {
 			sectionObjects [i] = sections [i].gameObject;
 		}
+
+		SortSections ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (SomethingWrong ()) {
+		if (!defaultsApplied && Time.time > 0.1f) {
+			defaultsApplied = true;
 
+			for (int i = 0; i < currStates.Length; i++) {
+				currStates [i] = true;
+			}
+		}
+
+		CheckColliders ();
+	}
+
+	void SortSections() {
+		SectionID[] shadow = new SectionID[sections.Length];
+
+		for (int i = 0; i < shadow.Length; i++) {
+			foreach (SectionID temp in sections) {
+				if (temp.thisSectionType == colliders [i].colliderType) {
+					shadow[i] = temp;
+					break;
+				}
+			}
+		}
+
+		sections = shadow;
+	}
+
+	void CheckColliders() {
+		for (int i = 0; i < currStates.Length; i++) {
+			if (currStates [i] != colliders [i].inside) {
+				currStates [i] = colliders [i].inside;
+
+				UpdateSection (i);
+
+			}
 		}
 	}
 
-	bool SomethingWrong() {
-		for (int i = 0; i < currStates.Length; i++) {
-			if (currStates [i] != colliders [i].inside) {
-				return true;
-			}
-			return false;
+	void UpdateSection(int section) {
+		sections [section].gameObject.SetActive (currStates [section]);
 	}
 }
